d471feb912ec04da7cc6921c5fb9d38a175a9ccb
AlinaGaifullina <kivi2177@gmail.com>
2025-04-26 16:11:45 +0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { ThemeProvider } from "./src/modules/theme/ThemeProvider.tsx";
 
 
 const App = () => {
   return (
-      <></>
+      <ThemeProvider >
+        <></>
+      </ThemeProvider>
   );
 }
 
