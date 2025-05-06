56d648f1c626386602c011761ffa2d4b6b87b9d5
kamilla11 <gafarova.kamilla@list.ru>
2025-05-06 13:22:48 +0300

    feat: add barcode scanner screen

--- src/screens/BarcodeScannerScreen.tsx
+++ src/screens/BarcodeScannerScreen.tsx
@@ -1,10 +1,28 @@
-import React from "react";
-import { Text } from "react-native";
+import { useCameraDevice, useCameraPermission } from "react-native-vision-camera";
+import NoCamera from "../components/scanner/NoCamera.tsx";
+import BarcodeScanner from "../components/scanner/BarcodeScanner.tsx";
+import NoCameraAccess from "../components/scanner/NoCameraAccess.tsx";
+import { useEffect } from "react";
+import { observer } from "mobx-react";
 
-const BarcodeScannerScreen = () => {
-  return <>
-    <Text>BarcodeScannerScreen</Text>
-  </>;
-};
+const BarcodeScannerScreen = observer(() => {
+  const device = useCameraDevice("back");
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission, requestPermission]);
+
+
+  if (!device) {
+    return <NoCamera />;
+  }
+  if (!hasPermission) {
+    return <NoCameraAccess />;
+  }
+  return <BarcodeScanner device={device} />;
+});
 
 export default BarcodeScannerScreen;
